ec41dacd0676644cfed7aafb14b0348e85bc01f7
Parin Kasabia <pkasabia0909@gmail.com>
2024-09-21 13:35:34 +1200

    finished portfolio v2

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -1,30 +1,38 @@
+"use client"
+import Link from "next/link"
 import React from 'react'
+
+import { ModeToggle } from '@/components/ui/ThemeToggle';
+import {PKLogoV2, LinkedinIcon, GithubIcon} from "@/config/icons";
+import { siteConfig } from "@/config/site";
+import { Button } from "@/components/ui/Button"
 
 const Footer = () => {
   return (
     <section id='footer' className='scroll-mt-16'>
         <div className="dark:bg-slate-950 bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-            <div className='container mx-auto px-4 md:px-6 py-6'> 
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portfolio</span>
-                    </a>
-                    <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://parinkasabia.com/" className="hover:underline">Parin Kasabia</a>. All Rights Reserved.</span>
-                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">About</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
-                        </li>
-                    </ul>
-            </div>
+            <div className='container mx-auto px-4 md:px-6 py-6 flex items-center justify-between'> 
+                <Link href="#" className="flex items-center gap-2" prefetch={false}>
+                    <PKLogoV2 className="h-10 w-10" />
+                    <span className="text-lg font-medium">Portfolio</span>
+                </Link>
+                <div className="text-sm text-gray-600 dark:text-gray-400 font-semibold text-center">© 2024 {" "}
+                    <Link href="https://www.parinkasabia.com" className="underline">Parin Kasabia</Link>. <br />All rights reserved.
+                    </div>
+
+                <div className="flex items-center gap-4">
+                    <Button variant="outline" size="icon">
+                        <Link aria-label="Twitter" href={siteConfig.links.linkedin}>
+                            <LinkedinIcon />
+                        </Link>
+                    </Button>
+                    <Button variant="outline" size="icon">
+                        <Link aria-label="Github" href={siteConfig.links.github}>
+                            <GithubIcon />
+                        </Link>
+                    </Button>
+                    <ModeToggle />
+                </div>
             </div>
         </div>
     </section>
